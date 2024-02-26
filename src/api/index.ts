a5b2d1723ff405500b78645bf5e8c343b1fd7667
Devi prasad <prasad20697@gmail.com>
2024-02-26 22:44:47 +0530

    main route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,18 @@
-import express from 'express';
+import express from "express";
 
-import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+import MessageResponse from "../interfaces/MessageResponse";
+import emojis from "./emojis";
+import todos from "./todo/todo.route";
 
 const router = express.Router();
 
-router.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏',
-  });
+router.get<{}, MessageResponse>("/", (req, res) => {
+	res.json({
+		message: "API - 👋🌎🌍🌏",
+	});
 });
 
-router.use('/emojis', emojis);
+router.use("/emojis", emojis);
+router.use("/todos", todos);
 
 export default router;
