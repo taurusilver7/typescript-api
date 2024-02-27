1758909c57582bc77399593e1c9c3c7fa576fea9
Devi prasad <prasad20697@gmail.com>
2024-02-27 18:02:47 +0530

    main todo route

--- src/api/todo/todo.route.ts
+++ src/api/todo/todo.route.ts
@@ -1,15 +1,12 @@
-import { Router } from "express";
-import Todo from "./todo.model";
+import { Request, Response, Router } from "express";
+import { Todo, TodoWithId, Todos } from "./todo.model";
 
 const router = Router();
 
-router.get<{}, Todo[]>("/", (req, res) => {
-	res.json([
-		{
-			content: "Learn Typescript",
-			done: true,
-		},
-	]);
+router.get("/", async (req: Request, res: Response<TodoWithId>) => {
+	const result = await Todos.find();
+	const todos = await result.toArray();
+	res.json(todos);
 });
 
 export default router;
