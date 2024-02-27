a1ce6024ea7de3265c5acd05490fd8d460bb56ee
Devi prasad <prasad20697@gmail.com>
2024-02-27 18:02:38 +0530

    todo data model

--- src/api/todo/todo.model.ts
+++ src/api/todo/todo.model.ts
@@ -1,10 +1,13 @@
 import * as z from "zod";
+import { db } from "../../db";
+import { WithId } from "mongodb";
 
 const Todo = z.object({
 	content: z.string().min(1),
 	done: z.boolean().default(false),
 });
 
-type Todo = z.infer<typeof Todo>;
+export type Todo = z.infer<typeof Todo>;
+export type TodoWithId = WithId<Todo>;
 
-export default Todo;
+export const Todos = db.collection<Todo>("todos");
