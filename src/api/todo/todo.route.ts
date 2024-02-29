b786f18c212ae288d7ecd2e426d2f8082ea592a9
Devi prasad <prasad20697@gmail.com>
2024-02-29 10:09:28 +0530

    route response json

--- src/api/todo/todo.route.ts
+++ src/api/todo/todo.route.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", async (req: Request, res: Response<TodoWithId>) => {
 	const result = await Todos.find();
 	const todos = await result.toArray();
-	res.json(todos);
+	// res.json(todos);
 });
 
 export default router;
