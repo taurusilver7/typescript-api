7565ecc3a0dc8ac59451a96d09ba3e3952fb4886
Devi prasad <prasad20697@gmail.com>
2024-03-03 17:23:57 +0530

    createOne test case

--- test/todo.test.ts
+++ test/todo.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /api/v1/todos", () => {
-	it("responds with an array of todos", () =>
+	it("responds with an array of todos", async () =>
 		request(app)
 			.get("/api/v1/todos")
 			.set("Accept", "application/json")
@@ -11,6 +11,21 @@ describe("GET /api/v1/todos", () => {
 			.expect(200)
 			.then((response) => {
 				expect(response.body).toHaveProperty("length");
+				expect(response.body.length).toBe(1);
+				expect(response.body[0]).toHaveProperty("content");
+				expect(response.body[0]).toHaveProperty("done");
+			}));
+});
+describe("POST /api/v1/todos", () => {
+	it("responds with an error if todo is invalid", async() =>
+		request(app)
+			.post("/api/v1/todos")
+			.set("Accept", "application/json")
+			.send({ content: "" })
+			.expect("Content-Type", /json/)
+			.expect(200)
+			.then((response) => {
+				expect(response.body.message).toHaveProperty("message"); 
 				expect(response.body.length).toBe(1);
 				expect(response.body[0]).toHaveProperty("content");
 				expect(response.body[0]).toHaveProperty("done");
