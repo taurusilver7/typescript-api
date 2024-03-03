ecdfcede6ba90def48c7ebe9f882d85c3fba08ca
Devi prasad <prasad20697@gmail.com>
2024-03-03 16:52:27 +0530

    findAll handler

--- src/api/todo/todo.handler.ts
+++ src/api/todo/todo.handler.ts
@@ -1,1 +1,17 @@
 // Controller function for a corresponding todo route.
+import { NextFunction, Request, Response } from "express";
+import { TodoWithId, Todos } from "./todo.model";
+
+export async function findAll(
+	req: Request,
+	res: Response<TodoWithId[]>,
+	next: NextFunction
+) {
+	try {
+		const result = await Todos.find();
+		const todos = await result.toArray();
+		res.json(todos);
+	} catch (error) {
+		next(error);
+	}
+}
