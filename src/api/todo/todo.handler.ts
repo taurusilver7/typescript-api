819aee1fd4ee9b6d826825dd4f2ed97f85877a98
Devi prasad <prasad20697@gmail.com>
2024-03-03 17:23:11 +0530

    createOne handler

--- src/api/todo/todo.handler.ts
+++ src/api/todo/todo.handler.ts
@@ -1,6 +1,7 @@
 // Controller function for a corresponding todo route.
 import { NextFunction, Request, Response } from "express";
-import { TodoWithId, Todos } from "./todo.model";
+import { TodoWithId, Todos, Todo } from "./todo.model";
+import { InsertOneResult } from "mongodb";
 
 export async function findAll(
 	req: Request,
@@ -11,6 +12,20 @@ export async function findAll(
 		const result = await Todos.find();
 		const todos = await result.toArray();
 		res.json(todos);
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function createOne(
+	req: Request<{}, InsertOneResult<Todo>, Todo>,
+	res: Response<TodoWithId>,
+	next: NextFunction
+) {
+	try {
+		const validateResult = await Todo.parse(req.body);
+		const insertResult = await Todo.insertOne(validateResult);
+      res.json(insertResult);
 	} catch (error) {
 		next(error);
 	}
