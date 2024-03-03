8323bf4dbb9b3411f0167b445db946e50ffea393
Devi prasad <prasad20697@gmail.com>
2024-03-03 16:52:33 +0530

    findAll route

--- src/api/todo/todo.route.ts
+++ src/api/todo/todo.route.ts
@@ -1,12 +1,8 @@
-import { Request, Response, Router } from "express";
-import { Todo, TodoWithId, Todos } from "./todo.model";
+import { Router } from "express";
+import * as TodoHandler from './todo.handler';
 
 const router = Router();
 
-router.get("/", async (req: Request, res: Response<TodoWithId>) => {
-	const result = await Todos.find();
-	const todos = await result.toArray();
-	// res.json(todos);
-});
+router.get("/", TodoHandler.findAll);
 
 export default router;
