7184f0dc0a383be56291258454a076d6ccdece4b
Devi prasad <prasad20697@gmail.com>
2024-03-04 10:09:12 +0530

    delete todo route

--- src/api/todo/todo.route.ts
+++ src/api/todo/todo.route.ts
@@ -1,16 +1,26 @@
 import { Router } from "express";
-import * as TodoHandler from './todo.handler';
+import * as TodoHandler from "./todo.handler";
 import { validateRequest } from "../../middlewares";
 import { ParamsWithId } from "../../interfaces/ParamsWithId";
-import {Todo} from "./todo.model";
+import { Todo } from "./todo.model";
 
 const router = Router();
 
 router.get("/", TodoHandler.findAll);
-router.post("/", validateRequest({body: Todo}), TodoHandler.createOne);
+router.post("/", validateRequest({ body: Todo }), TodoHandler.createOne);
 
-router.get("/:id", validateRequest({params: ParamsWithId,}), TodoHandler.findOne);
+router.get(
+	"/:id",
+	validateRequest({ params: ParamsWithId }),
+	TodoHandler.findOne
+);
 
-router.put("/:id", validateRequest({params: ParamsWithId, body: Todo}), TodoHandler.updateOne)
+router.put(
+	"/:id",
+	validateRequest({ params: ParamsWithId, body: Todo }),
+	TodoHandler.updateOne
+);
+
+router.delete("/:id", validateRequest({ params: ParamsWithId}), TodoHandler.deleteOne)
 
 export default router;
