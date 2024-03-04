30577b7f65f513d0e675653438cf93eafb74e396
Devi prasad <prasad20697@gmail.com>
2024-03-04 09:27:08 +0530

    findone handler

--- src/api/todo/todo.handler.ts
+++ src/api/todo/todo.handler.ts
@@ -1,7 +1,7 @@
 // Controller function for a corresponding todo route.
 import { NextFunction, Request, Response } from "express";
 import { TodoWithId, Todos, Todo } from "./todo.model";
-import { InsertOneResult } from "mongodb";
+import { InsertOneResult, ObjectId } from "mongodb";
 
 export async function findAll(
 	req: Request,
@@ -18,14 +18,37 @@ export async function findAll(
 }
 
 export async function createOne(
-	req: Request<{}, InsertOneResult<Todo>, Todo>,
+	req: Request<{}, TodoWithId, Todo>,
 	res: Response<TodoWithId>,
 	next: NextFunction
 ) {
 	try {
 		const validateResult = await Todo.parse(req.body);
 		const insertResult = await Todo.insertOne(validateResult);
-		res.json(insertResult);
+		res.status(200);
+		res.json({
+			_id: insertResult.insertdId,
+			...req.body,
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function findOne(
+	req: Request<TodoWithId, {}>,
+	res: Response<TodoWithId>,
+	next: NextFunction
+) {
+	try {
+		const result = await Todo.findOne({
+			_id: new ObjectId(req.params?.id),
+		});
+		if (!result) {
+			res.status(404);
+			throw new Error(`Todo with id "${req.params?.id}" not found!`);
+		}
+		res.json(result);
 	} catch (error) {
 		next(error);
 	}
