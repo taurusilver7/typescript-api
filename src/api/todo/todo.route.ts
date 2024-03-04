0411e00fab059149417c312c8865b91dc4cc9e7b
Devi prasad <prasad20697@gmail.com>
2024-03-04 09:55:58 +0530

    findOne route

--- src/api/todo/todo.route.ts
+++ src/api/todo/todo.route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import * as TodoHandler from './todo.handler';
+import { validateRequest } from "../../middlewares";
+import { ParamsWithId } from "../../interfaces/ParamsWithId";
+import {Todo} from "./todo.model";
 
 const router = Router();
 
 router.get("/", TodoHandler.findAll);
 router.post("/", TodoHandler.createOne);
 
-router.get("/:id", TodoHandler.findOne);
+router.get("/:id", validateRequest({params: ParamsWithId,}), TodoHandler.findOne);
 
 export default router;
